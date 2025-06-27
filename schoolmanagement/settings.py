3b1679e3959c2caa3a01070fd9318314e0a4a6b7
loke <lokendar384@gmail.com>
2025-06-27 19:21:50 +0545

    mainappurlconfig

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    
 ]
 
 ROOT_URLCONF = 'schoolmanagement.urls'
