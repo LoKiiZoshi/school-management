597b1f8788b57e8b38adf194f4370a3134153f79
LoKiiZoshi <lokendarjoshi384@gmail.com>
2025-06-30 19:06:59 +0545

    some route create or register

--- teachers/urls.py
+++ teachers/urls.py
@@ -1,32 +1,12 @@
-from rest_framework import viewsets,status,filters
-from rest_framework.decorators import action
-from rest_framework.response import responses
-from django_filters.rest_framework import DjangoFilterBackend
-from django.db.models import Count,Avg , Sum
-from datetime import date, timedelta
-from .models import DepartmentSerializer, TeacherSerializer,TeacherCreateSerializer,TeacherAttendanceSerializer
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import DepartmentViewSet, TeacherViewSet, TeacherAttendanceViewSet
 
+router = DefaultRouter()
+router.register(r'departments', DepartmentViewSet)
+router.register(r'', TeacherViewSet, basename='teacher')
+router.register(r'attendance', TeacherAttendanceViewSet)
 
-class DepartmentViewSet(viewsets.ModelViewSet):
-    queryset = Department.object.all()
-    serializer_class = DepartmentSerializer
-    filter_backends = [ djangoFilterBakends,filter.SearchFilter]
-    search_fields = ['name','code','description']
-    
-    
-    @action(detail=True,methods=['get'])
-    def teachers(self, request, pk = None):
-        department = self.get_object()
-        teacher = department.teacher.filters(is_active = True)
-        serializer = TeacherSerializer(teachers,many = True)
-        
-    @action(detail =True, method = ['get'])
-    def statistics(self,request,pk = Npne):
-        department = self.get_object()
-        teachr = department.teachers.filter(is_activate= True)
-        
-        stats = {
-            'total_teachers':teachers.count(),
-            'avg_experience':teaches.aggregate(Avg('experince_years'))['experience_years_avg'] or 0,
-            
-        }
+urlpatterns = [
+    path('', include(router.urls)),
+]
