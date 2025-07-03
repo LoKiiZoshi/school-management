58e492f0202faa4b00983c55586d98eca8ebc598
LoKiiZoshi <lokendarjoshi384@gmail.com>
2025-07-03 18:36:39 +0545

    up

--- academics/tests.py
+++ academics/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import Subject
 
-# Create your tests here.
+class SubjectModelTest(TestCase):
+    def test_subject_creation(self):
+        subject = Subject.objects.create(name="Math", code="MATH101")
+        self.assertEqual(subject.name, "Math")
