7c7f1d16360bd2a97aa4e1cd85772952abfd093e
LoKiiZoshi <lokendarjoshi384@gmail.com>
2025-07-03 05:50:20 +0545

    some add not complate

--- academics/views.py
+++ academics/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Count, Avg, Q
+from datetime import date, timedelta
+from .models import ExamType, Exam, Result, Assignment, Submission
+from .serializers import (ExamTypeSerializer, ExamSerializer, ResultSerializer, 
+                         AssignmentSerializer, SubmissionSerializer)
 
-# Create your views here.
+class ExamTypeViewSet(viewsets.ModelViewSet):
+    queryset = ExamType.objects.all()
+    serializer_class = ExamTypeSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['name', 'description']
+    filterset_fields = 
