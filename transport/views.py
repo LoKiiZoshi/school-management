128560767da80830c875c97044b5a011532b5ae5
LoKiiZoshi <lokendarjoshi384@gmail.com>
2025-07-04 15:26:28 +0545

    some views create

--- transport/views.py
+++ transport/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Vehicle, Booking
 
-# Create your views here.
+@api_view(['GET'])
+def vehicle_list(request):
+    vehicles = Vehicle.objects.all()
+    return Response({'vehicles': [{'id': v.id, 'name': v.name} for v in vehicles]})
+
+@api_view(['GET'])
+def vehicle_detail(request, id):
+    vehicle = Vehicle.objects.get(id=id)
+    return Response({'id': vehicle.id, 'name': vehicle.name})
+
+@api_view(['GET'])
+def booking_list(request):
+    bookings = Booking.objects.all()
+    return Response({'bookings': [{'id': b.id, 'vehicle_id': b.vehicle.id} for b in bookings]})
+
+@api_view(['GET'])
+def booking_detail(request, id):
+    booking = Booking.objects.get(id=id)
+    return Response({'id': booking.id, 'vehicle_id': booking.vehicle.id})
